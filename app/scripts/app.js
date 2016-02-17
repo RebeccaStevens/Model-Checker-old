e9fb75a602a8a9b9498452e530e2d967e1458713
slaymaster3000 <slaymaster3000@gmail.com>
2016-02-17 13:28:17 +1300

    explicitly calling render fix for MS Edge, no longer needed due to other fixes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -89,9 +89,6 @@
             setTimeout(function() {
               app.set('automata', automata);
 
-              // explicitly call render - fix for MS Edge, but extra overhead on other browsers
-              app.$['automata-renderer'].render();
-
               // listen for each rendered event.
               // once all automata have been rendered, log the results and stop listening.
               var automataRendered = 0;
